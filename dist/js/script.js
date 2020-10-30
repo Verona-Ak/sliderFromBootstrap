62ea8c43e53f09194dbe067bfff82b1e1a953742
Veronika1703 <akulova.veronika17@gmail.com>
2020-10-30 23:35:42 +0700

    created slider from Bootctrap

---
+++ dist/js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+window.addEventListener('DOMContentLoaded', ()=> {
+    $('.carousel').carousel({
+        ride: false,
+        touch: true,
+        keyboard: true,
+        wrap: true,
+        interval: false
+
+    });
+
+    let arrowL = document.querySelector('.carousel-control-prev-icon'),
+        arrowR = document.querySelector('.carousel-control-next-icon'),
+        width = '60px',
+        height = '60px';
+
+    function styleForArrows(l, r, w = width, h = height) {
+        l.style.cssText = `background-image: url(img/left_arrow.png); width: ${w}; height: ${h};`;
+        r.style.cssText = `background-image: url(img/right_arrow.png); width: ${w}; height: ${h};`;
+    }
+    styleForArrows(arrowL, arrowR);
+
+    if(window.matchMedia('(max-width: 460px)').matches){
+        document.querySelector('.carousel-control-next').style.width = '24%';
+        document.querySelector('.carousel-control-prev').style.width = '24%';
+    }
+
+});
